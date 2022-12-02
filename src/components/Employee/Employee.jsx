9d7924cdf5620faa30af6657bf6cc71f3a0741c4
CHRIS BIGLAND <christopherbigland@gmail.com>
2022-12-02 10:52:20 +0000

    added onclick to buttons to change state and make them count

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Employee.module.scss'
 
 const Employee = (props) => {
 
     const {name, role} = props.employee;
+    const [count, setCount] = useState(1);
 
   return (
     <React.Fragment>
     <div className={styles.employee}>
         <div className={styles.display}>
-            <span>0</span>
+            <span>{count}</span>
         </div>
         <div className={styles.plusMinus}>
             <span>
-                <p>-</p></span>
+                <button onClick={() => setCount(count - 1)}>-</button></span>
             <span>
-                <p>+</p></span>
+                <button onClick={() => setCount(count + 1)}>+</button></span>
         </div>
+
+        {/* <button onClick={() => setCount(count - 1)}>-</button></span> */}
         <div className={styles.info}>
             <p className={styles.name}>Name: {name}</p>
             <p className={styles.role}>Role: {role}</p>
