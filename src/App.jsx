f7155df4c7198ccec960407af3f2cf1cbf7f140a
CHRIS BIGLAND <christopherbigland@gmail.com>
2022-12-05 17:21:49 +0000

    first place in top 3 performers now identifies correct index number

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import styles from './App.module.scss'
-import Employee from './components/Employee';
-import TicketTotal from './components/TicketTotal';
-import TopPerformers from './components/TopPerformers';
-import team from './data/data'
+import React, { useState } from "react";
+import { isCompositeComponent } from "react-dom/test-utils";
+import styles from "./App.module.scss";
+import Employee from "./components/Employee";
+import TicketTotal from "./components/TicketTotal";
+import TopPerformers from "./components/TopPerformers";
+import team from "./data/data";
 
 const App = () => {
-  const [totalTicketNo, setTotalTicketNo] = useState(0)
-  const [numberOne, setNumberOne] = useState("")
-  const [numberTwo, setNumberTwo] = useState("")
-  const [numberThree, setNumberThree] = useState("")
+  const [totalTicketNo, setTotalTicketNo] = useState(0);
+  const [numberOne, setNumberOne] = useState("");
+  const [numberTwo, setNumberTwo] = useState("");
+  const [numberThree, setNumberThree] = useState("");
   // first place to be name of someone calculated checking an array
   const [allScores, setAllScores] = useState([]);
-  
+
   const getEmployee = (employee, index) => (
     <Employee
       employee={employee}
@@ -27,34 +28,53 @@ const App = () => {
     />
   );
 
- 
+  const allScoresCopy = [...allScores];
+  // console.log(allScoresCopy)
+  //{console.log(Math.max(allScoresCopy))};
 
+  let max = allScoresCopy[0];
 
-// above - pass count back up like with totalTicketNo?
+  for (let i = 0; i < allScoresCopy.length; i++) {
+    if (allScoresCopy[i] > max) {
+      max = allScoresCopy[i];
+    }
+  }
+  const positionOne = allScoresCopy.indexOf(max);
+  // console.log(allScoresCopy.indexOf(max))
+  console.log(positionOne);
+  //console.log(max.indexOf())
+
+  // const sortedNumbers = allScoresCopy.sort((a, b) => {
+  // return b - a;
+  //    });
+  //console.log(...sortedNumbers);
+
+  // above - pass count back up like with totalTicketNo?
   return (
-  <React.Fragment>
-    <h1 className={styles.heading}>Ticket Tracker</h1>
-    <div className={styles.ticketTotal}>
-    <TicketTotal totalTicketNo={totalTicketNo} />
-    </div>
-    <div className={styles.dateAndTotal}>
-    <h2 className={styles.date}>Today's Date:  {`${new Date().toLocaleString()}`}</h2>
+    <React.Fragment>
+      <h1 className={styles.heading}>Ticket Tracker</h1>
+      <div className={styles.ticketTotal}>
+        <TicketTotal totalTicketNo={totalTicketNo} />
+      </div>
+      <div className={styles.dateAndTotal}>
+        <h2 className={styles.date}>
+          Today's Date: {`${new Date().toLocaleString()}`}
+        </h2>
+      </div>
+      <div>
+        <h2 className={styles.topThree}>Top 3 performers:</h2>
 
-    </div>
-    <div>
-      <h2 className={styles.topThree}>Top 3 performers:</h2>
-     
-      <h2>1: {numberOne}</h2>
-      <h2>2: {numberTwo}</h2>
-      <h2>3: {numberThree}</h2>
-    </div>
-    <section className={styles.content}>
-      {team.map((person, index) => getEmployee(person, index))} 
-      {/* <TopPerformers /> */}
-  </section>
-  </React.Fragment>
+        <h2>1: {positionOne}</h2>
+        <h2>2: {numberTwo}</h2>
+        <h2>3: {numberThree}</h2>
+      </div>
+      <section className={styles.content}>
+        {team.map((person, index) => getEmployee(person, index))}
+        {/* <TopPerformers /> */}
+      </section>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
 
@@ -63,6 +83,5 @@ export default App;
 //https://www.folkstalk.com/tech/how-to-define-array-in-react-js-with-code-examples/
 // https://www.w3schools.com/react/react_es6_array_methods.asp
 
-
 // ideas - access employee.count, store that as a state for each employee and put those into an array to work out top numbers
-// store the employee name and their count as object items in an array. 
+// store the employee name and their count as object items in an array.
--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -1,95 +1,108 @@
-import React, { useState, useEffect } from 'react'
-import { scryRenderedDOMComponentsWithClass } from 'react-dom/test-utils';
-import styles from './Employee.module.scss'
+import React, { useState, useEffect } from "react";
+import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
+import styles from "./Employee.module.scss";
 
-const Employee = ({totalTicketNo, setTotalTicketNo, employee, numberOne, setNumberOne, index, allScores, setAllScores}) => {
+const Employee = ({
+  totalTicketNo,
+  setTotalTicketNo,
+  employee,
+  numberOne,
+  setNumberOne,
+  index,
+  allScores,
+  setAllScores,
+}) => {
+  // const {name, role} = props.employee;
+  const [count, setCount] = useState(0);
+  const subtraction = count > 0 ? count - 1 : 0;
+  const totalSubtraction = count > 0 ? totalTicketNo - 1 : totalTicketNo;
+  //const allScoresTest = allScores.length === 1 ? allScores : ...allScores;
 
-    // const {name, role} = props.employee;
-    const [count, setCount] = useState(0);
-    const subtraction = count > 0 ? count - 1 : 0;
-    const totalSubtraction = count > 0 ? totalTicketNo - 1 : totalTicketNo;
-    //const allScoresTest = allScores.length === 1 ? allScores : ...allScores;
+  // `${props.setTotalTicketNo(${props.totalTicketNo} + count)}`
+  const handlePlusClick = () => {
+    setCount(count + 1);
+    setTotalTicketNo(totalTicketNo + 1)((allScores[index] = count + 1));
 
-// `${props.setTotalTicketNo(${props.totalTicketNo} + count)}`
-    const handlePlusClick = () => {
-        setCount(count + 1)
-        setTotalTicketNo(totalTicketNo + 1)
-        console.log(allScores[index])
-        (allScores[index] = count + 1)
-        setAllScores([...allScores]);
-    }
+    setAllScores([...allScores]);
+    console.log(allScores);
+  };
 
-    //const handlePlusClick = () => {
-     //   setCount(count + 1)
-     //   setTotalTicketNo(totalTicketNo + 1)
-     //   setAllScores([...allScores, (allScores[index] = count + 1)]);
-   // }
+  //const handlePlusClick = () => {
+  //   setCount(count + 1)
+  //   setTotalTicketNo(totalTicketNo + 1)
+  //   setAllScores([...allScores, (allScores[index] = count + 1)]);
+  // }
 
- //   useEffect(() => {
-   //     if (allScores.length === 1) {
-      //    console.log('equals 1');
-     // } 
-        //else {
-        //  console.log('No threshold reached.');
-       // }
-     // }, [count]);
-    // finding place in array that needs to be updated
-    //copying array and updating item. 
-    
-    const handleMinusClick = () => {
-        setCount(subtraction)
-        setTotalTicketNo(totalSubtraction)
-        (allScores[index] = subtraction)
-        setAllScores([...allScores]);
-    }
+  //   useEffect(() => {
+  //     if (allScores.length === 1) {
+  //    console.log('equals 1');
+  // }
+  //else {
+  //  console.log('No threshold reached.');
+  // }
+  // }, [count]);
+  // finding place in array that needs to be updated
+  //copying array and updating item.
 
-    const calculateRanking = (count) => {
-        //could I get each count to add to an array as they are being mapped and then apply logic to work out position in that array?
-        const indivCount = count
-        return indivCount
-    }
+  const handleMinusClick = () => {
+    setCount(subtraction);
+    setTotalTicketNo(totalSubtraction)((allScores[index] = subtraction));
+    setAllScores([...allScores]);
+  };
 
-    const employeeName = employee.name
-    console.log(employeeName)
-   // let numbersArr = [{}]
-// create an array of objects here? with employee name and count?
-    //const getEmployeeNumbers = (employeeName, count) => {
-      // const employeeNameAndNumber = {
-       // name: employeeName,
-      //  number: count
-     //  }
-     //   console.log(employeeNameAndNumber)
-       /// console.log(employee.name)
-        //numbersArr.push(employee)
-        //numbersArr.push(count)
-       // return employeeNameAndNumber
-    //  }
-    console.log(count)
+  const calculateRanking = (count) => {
+    //could I get each count to add to an array as they are being mapped and then apply logic to work out position in that array?
+    const indivCount = count;
+    return indivCount;
+  };
+
+  const employeeName = employee.name;
+  console.log(employeeName);
+  // let numbersArr = [{}]
+  // create an array of objects here? with employee name and count?
+  //const getEmployeeNumbers = (employeeName, count) => {
+  // const employeeNameAndNumber = {
+  // name: employeeName,
+  //  number: count
+  //  }
+  //   console.log(employeeNameAndNumber)
+  /// console.log(employee.name)
+  //numbersArr.push(employee)
+  //numbersArr.push(count)
+  // return employeeNameAndNumber
+  //  }
+  console.log(count);
 
   return (
     <React.Fragment>
-    <div className={styles.employee}>
+      <div className={styles.employee}>
         <div className={styles.display}>
-            <span>{count}</span>
+          <span>{count}</span>
         </div>
         <div className={styles.plusMinus}>
-            <span>
-                <button className={styles.btn} onClick={() => handleMinusClick()}>-</button></span>
-            <span>
-                <button className={styles.btn} onClick={() => handlePlusClick()}>+</button></span>
+          <span>
+            <button className={styles.btn} onClick={() => handleMinusClick()}>
+              -
+            </button>
+          </span>
+          <span>
+            <button className={styles.btn} onClick={() => handlePlusClick()}>
+              +
+            </button>
+          </span>
 
-            <span>
-              {/*  <button onClick={() => getEmployeeNumbers()}>work out ranking </button> */ }
-            </span>
+          <span>
+            {/*  <button onClick={() => getEmployeeNumbers()}>work out ranking </button> */}
+          </span>
         </div>
 
         <div className={styles.info}>
-            <p className={styles.name}>Name: {employee.name}</p>
-            <p className={styles.role}>Role: {employee.role}</p>
+          <p className={styles.name}>Name: {employee.name}</p>
+          <p className={styles.role}>Role: {employee.role}</p>
         </div>
-    </div>
+      </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Employee
+export default Employee;
