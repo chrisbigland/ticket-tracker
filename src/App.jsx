3ed3ca316cae02b85d8e49ceec0766b0e14d281c
CHRIS BIGLAND <christopherbigland@gmail.com>
2022-12-07 16:49:17 +0000

    top three is now empty on page load and only updates when people add to trackers

--- src/App.jsx
+++ src/App.jsx
@@ -9,87 +9,18 @@ import team from "./data/data";
 const App = () => {
   const [totalTicketNo, setTotalTicketNo] = useState(0);
 
- // const [allScores, setAllScores] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-
   const getEmployee = (employee, index) => (
     <Employee
       employee={employee}
       key={employee.id}
       totalTicketNo={totalTicketNo}
       setTotalTicketNo={setTotalTicketNo}
-     // allScores={allScores}
-     // setAllScores={setAllScores}
       index={index}
       team={team}
       handleAdd={handleAdd}
       handleMinus={handleMinus}
     />
   );
-
- // const allScoresCopy = [...allScores];
-
-//  let max = 0
- // let maxScore = 0
- // for (let i = 0; i < allScoresCopy.length; i++) {
-  //  if (allScoresCopy[i] > maxScore) {  // if the value of the item is bigger than maxScore, max is set to the index of this item
-  //    max = allScoresCopy.indexOf(allScoresCopy[i]);
-   //   maxScore = allScoresCopy[i];
-   // }
- // }
-//console.log("max is", max)
-
-//let secondLargestScore = 0;
-//const [secondLargestScore, setSecondLargest] = useState(0)
-  //let secondLargest = 0; //does this reset secondLargest back to 0 on the rerender?
-  //for (let i = 0; i < allScoresCopy.length; i++) {
-   // if (allScoresCopy.indexOf(allScoresCopy[i]) != max && allScoresCopy[i] >= secondLargestScore) {
-    //  console.log("second largest score is...", secondLargestScore)
-      //console.log("this index is not the same index as max and this item is larger than secondLargest Score") // if the index of the item I'm looking at in this iteration is not the same as max (an index) and the value is bigger than the second Largest Score
-     // secondLargest = allScoresCopy.indexOf(allScoresCopy[i]); // we set secondLargest to the index of the item we're looking at
-     // secondLargestScore = allScoresCopy[i];
-      //setSecondLargest(allScoresCopy[i]);
-
-      //console.log("allScoresCopy item index is set to", allScoresCopy.indexOf(allScoresCopy[i]))
-      //console.log("allScoresCopy item value is", allScoresCopy[i])
- //  }
- //  else {
- //   console.log("second largest score is...", secondLargestScore)
-    //console.log("else..... - this index is either the same index as max or this item is NOT larger than secondLargest Score")
-    //console.log("e;se.... - allScoresCopy item index is set to", allScoresCopy.indexOf(allScoresCopy[i]))
-    //console.log("else....DIDN'T WORK - allScoresCopy item value is", allScoresCopy[i])
- //  }
-//  }
-
-// issue - want to secondLargest to remain as the value when the numbers tie
-
-//console.log("secondLargest is", secondLargest)
-//let thirdLargestScore = 0
- // let thirdLargest = 0
-
- // for (let i = 0; i < allScoresCopy.length; i++) {
- //   if (
-   //   allScoresCopy.indexOf(allScoresCopy[i]) != max && allScoresCopy.indexOf(allScoresCopy[i]) != secondLargest && allScoresCopy[i] > thirdLargest) {
-  //    thirdLargest = allScoresCopy.indexOf(allScoresCopy[i]);
-   //   thirdLargestScore = allScoresCopy[i];
-  //  }
- // }
-
- // const positionOneTernary = maxScore > 0 ? team[max]?.name : "";
- // console.log("positionOneTernary is", positionOneTernary)
- // console.log("team is", team)
- // console.log("team max name is", team[max].name)
- // console.log("maxScore is", maxScore)
- // const positionTwoTernary = secondLargestScore > 0 ? team[secondLargest]?.name : "";
- // if (secondLargestScore === maxScore && secondLargestScore > 0) { 
-  //  console.log("secondLargestScore is equal to maxScore")
-   // positionTwoTernary = team[secondLargest]?.name
- // }
- // console.log("positionTwoTernary is", positionTwoTernary)
- // console.log("team secondLargest name is", team[secondLargest].name)
- // console.log("secondLargestScore is", secondLargestScore)
- // const positionThreeTernary =
-  //  thirdLargestScore > 0 ? team[thirdLargest]?.name : "";
-
 
 
   let objectDate = new Date();
@@ -98,8 +29,6 @@ const App = () => {
   let year = objectDate.getFullYear();
   let todaysDate = `${day}/${month}/${year}`;
 
-
-  //////COPIED CODE
   const [employees, setEmployees] = useState(team); // sets employees state to the team array of objects
 
   const handleAdd = (employeeId) => {       // handleAdd to be used in the Employee component when number is increased. The specific employeeID is passed through so we're dealing with right employee
@@ -130,8 +59,6 @@ const App = () => {
   }
 
   console.log(employees)
-  // use employees state to get the top performers
-  // sort so high scores are first
 
 
   const sortedEmployees = [...employees].sort((a, b) => {
@@ -149,28 +76,14 @@ const App = () => {
     return b.score - a.score;
   });
   console.log(sortedEmployees)
-  // top performers are...
-  // sortedEmployees[0]
-  // sortedEmployees[1]
-  // sortedEmployees[2]
 
-//  return (
-   // <div>
-   //   {sortedEmployees[0].name}
-   //   {sortedEmployees[0].score}
-   //   {employees.map((teamMember) => (
-    //    <div key={teamMember.name}>
-   //       <p>Name: {teamMember.name}</p>
-     //     <p>Count: {teamMember.score ?? 0}</p>
-         {/*needs to be in component*/}// <button onClick={() => handleAdd(teamMember.id)}>Add</button>
-  //      </div>
-  //    ))}
- //   </div>
- // );
-//};
 console.log(sortedEmployees[0])
 console.log(sortedEmployees[1])
 console.log(sortedEmployees[2])
+
+const positionOne = sortedEmployees[0].score === 0 ? "" : sortedEmployees[0].name;
+const positionTwo = sortedEmployees[1].score === 0 ? "" : sortedEmployees[1].name;
+const positionThree = sortedEmployees[2].score === 0 ? "" : sortedEmployees[2].name;
 
 employees.map((employee) => {
   if (employee.score === undefined) {
@@ -190,15 +103,13 @@ employees.map((employee) => {
         <h2 className={styles.topThreeTitle}>Top 3 performers:</h2>
 
         <h2 className={styles.pos1}>
-          1: {sortedEmployees[0].name}
-          {/* ? here shows that if it's not defined then ignore it*/}
+          1: {positionOne}
         </h2>
-        <h2 className={styles.pos2}>2: {sortedEmployees[1].name}</h2>
-        <h2 className={styles.pos3}>3: {sortedEmployees[2].name}</h2>
+        <h2 className={styles.pos2}>2: {positionTwo}</h2>
+        <h2 className={styles.pos3}>3: {positionThree}</h2>
       </div>
       <section className={styles.content}>
         {team.map((person, index) => getEmployee(person, index))}
-      {/*  {team.map((employee) => getFirstPlace(employee))}*/ }
         {/* <TopPerformers /> */}
       </section>
     </React.Fragment>
@@ -215,14 +126,6 @@ export default App;
 // ideas - access employee.count, store that as a state for each employee and put those into an array to work out top numbers
 // store the employee name and their count as object items in an array.
 
-// to do: fix bug where top person is '0' if everything is '0' before them
-// work out how to change the number to a name - using data?
-// work out how to add second and third places
-// work out how to shorten date to just date
-// ask about drop-down menu - worth doing?
-// filter out undefineds (this should fix issue of ranking order being wrong)
-// THEN get to the root cause of why the undefineds were appearing at beginning of array.
-// clear out console logs
-// sort out ranking issue where if the second place has same score as first then first person in array is assigned second place
+// to do: make top performers component
 // improve variable names/amend similar ones
-// remove the first place person appearing after the last tracker on the browser - I believe it's because of the map
+// make it so that the top 3 are empty at the beginning of the day. 
--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -1,63 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
 import styles from "./Employee.module.scss";
 
 const Employee = ({
-  totalTicketNo,
-  setTotalTicketNo,
   employee,
-  index,
-  allScores,
-  setAllScores,
   handleAdd, 
   handleMinus
 }) => {
-  console.log("setTotalTicketNo is", setTotalTicketNo);
-  // const {name, role} = props.employee;
- // const [count, setCount] = useState(0);
-  //const subtraction = count > 0 ? count - 1 : 0;
- //const totalSubtraction = count > 0 ? totalTicketNo - 1 : totalTicketNo;
-  //const allScoresTest = allScores.length === 1 ? allScores : ...allScores;
 
-  // `${props.setTotalTicketNo(${props.totalTicketNo} + count)}`
- // const handlePlusClick = () => {
-   // setCount(count + 1);
-   // setTotalTicketNo((prev) => prev + 1); // previously this was totalTicketNo + 1. This just adds to the previous state
-   // allScores[index] = count + 1; //DO SOMETHING HERE?
-    // above sets the number of the array item to be the same as the number of the count (as it also does count + 1)
-    // it does this by targetting the same index as is being looped through on the map.
-  //  setAllScores([...allScores]);
-   // console.log(allScores);
-    // getFirstPlace();
-  //};
-
-  // when we click we:
-  // set the count state
-  // set the total ticket number state (this is shown on the browser)
-  //set each array item in all scores
-
-  //const handlePlusClick = () => {
-  //   setCount(count + 1)
-  //   setTotalTicketNo(totalTicketNo + 1)
-  //   setAllScores([...allScores, (allScores[index] = count + 1)]);
-  // }
-
-  //   useEffect(() => {
-  //     if (allScores.length === 1) {
-  //    console.log('equals 1');
-  // }
-  //else {
-  //  console.log('No threshold reached.');
-  // }
-  // }, [count]);
-  // finding place in array that needs to be updated
-  //copying array and updating item.
-
-  //const handleMinusClick = () => {
-  //  setCount(subtraction);
-   // setTotalTicketNo(totalSubtraction)((allScores[index] = subtraction));
-   // setAllScores([...allScores]);
-  //};
 
   const display = employee.score > 0 ? employee.score : 0;
 
@@ -78,10 +28,6 @@ const Employee = ({
             <button className={styles.btn} onClick={() => handleAdd(employee.id)}>
               +
             </button>
-          </span>
-
-          <span>
-            {/*  <button onClick={() => getEmployeeNumbers()}>work out ranking </button> */}
           </span>
         </div>
 
